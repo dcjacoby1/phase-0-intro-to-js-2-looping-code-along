f97392661fd152ace2599cbb2b58cafb44a7cd9b
Dan Jacoby <jacobydan01@gmail.com>
2024-02-08 23:22:23 -0500

    final

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(stringName,eventName) {
+    const messages = [];
+    for (let i = 0; i < stringName.length; i++) {
+        messages[i] = `Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return messages;
+}
+
+function countDown(initialNumber) {
+    while (initialNumber >= 0) {
+        console.log(initialNumber);
+        initialNumber --;
+    }
+}
